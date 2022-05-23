b20e1b08b336d7bec8c4e5a3e856004513f1d23b
ChatIsMut1ed <ala44yacoub@gmail.com>
2022-05-23 15:49:12 +0100

    first commit

---
+++ src/AppTopbar.js
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import { Toast } from "primereact/toast";
+import { Menu } from "primereact/menu";
+export const AppTopbar = (props) => {
+    const menu = useRef(null);
+    const toast = useRef(null);
+    const items = [
+        {
+            label: "Paramétre",
+            items: [
+                {
+                    label: "Profile",
+                    icon: "pi pi-user-edit",
+                    command: () => {
+                        window.location.hash = "/profile";
+                    },
+                },
+                {
+                    label: "Logout",
+                    icon: "pi pi-times",
+                    command: () => {
+                        window.location.hash = "/login";
+                    },
+                },
+            ],
+        },
+    ];
+    const handleToggle = (event) => {
+        menu.current.toggle(event);
+        props.onMobileSubTopbarMenuClick(event);
+    };
+    return (
+        <div className="layout-topbar">
+            <Link to="/" className="layout-topbar-logo">
+                <img src={props.layoutColorMode === "light" ? "/images/etudagro.jpg" : "/images/etudagro.jpg"} alt="logo" />
+                <span>Agro Etudes</span>
+            </Link>
+
+            <button type="button" className="p-link  layout-menu-button layout-topbar-button" onClick={props.onToggleMenuClick}>
+                <i className="pi pi-bars" />
+            </button>
+
+            <button type="button" className="p-link layout-topbar-menu-button layout-topbar-button" onClick={props.onMobileTopbarMenuClick}>
+                <i className="pi pi-ellipsis-v" />
+            </button>
+
+            <ul className={classNames("layout-topbar-menu lg:flex origin-top", { "layout-topbar-menu-mobile-active": props.mobileTopbarMenuActive })}>
+                {/* <li>
+                    <button className="p-link layout-topbar-button" onClick={props.onMobileSubTopbarMenuClick}>
+                        <i className="pi pi-calendar" />
+                        <span>Events</span>
+                    </button>
+                </li>
+                <li>
+                    <button className="p-link layout-topbar-button" onClick={props.onMobileSubTopbarMenuClick}>
+                        <i className="pi pi-cog" />
+                        <span>Settings</span>
+                    </button>
+                </li> */}
+                <li>
+                    <button className="p-link layout-topbar-button" onClick={(event) => handleToggle(event)} aria-controls="popup_menu" aria-haspopup>
+                        <i className="pi pi-user" />
+                        <span>Profile</span>
+                    </button>
+                    <Menu model={items} popup ref={menu} id="popup_menu" />
+                </li>
+            </ul>
+        </div>
+    );
+};
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import "react-app-polyfill/ie11";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+//import * as serviceWorker from './serviceWorker';
+import { HashRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
+// Create a client
+const queryClient = new QueryClient();
+ReactDOM.render(
+    <HashRouter>
+        <ScrollToTop>
+            <QueryClientProvider client={queryClient}>
+                <App></App>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </ScrollToTop>
+    </HashRouter>,
+    document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
